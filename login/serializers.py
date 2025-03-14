09a58f545094e1f38a7587df5188efcc0b205014
Rojasjuan08 <juan.rojas.quintero@correounivalle.edu.co>
2025-03-14 15:49:14 -0500

    Implementacion del registro de usuario

---
+++ login/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+from .models import RegistrarUsuario
+
+class RegistroUsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RegistrarUsuario
+        fields = ["cedula", "first_name", "last_name", "email", "password"]
+
+    # Encriptar la contraseña antes de guardarla
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
