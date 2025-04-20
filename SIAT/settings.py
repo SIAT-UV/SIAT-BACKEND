6844e4ac25e3e494149199996d6d0da4920cc9b4
RonaldoMunoz <yeifer.munoz@correounivalle.edu.co>
2025-04-19 23:23:43 -0500

    Se agregaron archivos necesarios para el despliegue

--- SIAT/settings.py
+++ SIAT/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = 'django-insecure-k+epr)cir*7c%=^)1r---uec(up)*t=^v$h&*g_k!nu_)rth(-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -79,6 +79,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'SIAT.urls'
@@ -178,6 +179,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
