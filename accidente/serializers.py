8e77ef10a3662452d234e80b2472e7895cd6941b
Rojasjuan08 <juan.rojas.quintero@correounivalle.edu.co>
2025-05-04 13:29:57 -0500

    Envio de coordenadas para graficacion del mapa de calor

--- accidente/serializers.py
+++ accidente/serializers.py
@@ -1,4 +1,3 @@
-from rest_framework_gis.serializers import GeoFeatureModelSerializer
 from rest_framework import serializers
 from .models import Accidente
 
@@ -24,9 +23,27 @@ class AccidenteSerializer(serializers.ModelSerializer):
             'coordenada_geografica',
             'imagen',
         )
-
     def create(self, validated_data):
         # Crear el objeto Accidente directamente
         accidente = Accidente.objects.create(**validated_data)
         return accidente
+class AccidenteListSerializer(serializers.ModelSerializer):
+    lat = serializers.SerializerMethodField()  # Latitud calculada
+    lon = serializers.SerializerMethodField()  # Longitud calculada
+
+    class Meta:
+        model = Accidente
+        fields = ('lat', 'lon')
+
+    def get_lat(self, obj):
+        if obj.coordenada_geografica:
+            return obj.coordenada_geografica.y
+        return None
+
+    def get_lon(self, obj):
+        if obj.coordenada_geografica:
+            return obj.coordenada_geografica.x
+        return None
+
+    
 
--- accidente/views.py
+++ accidente/views.py
@@ -1,10 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import AccidenteSerializer
+from .serializers import AccidenteSerializer, AccidenteListSerializer
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.permissions import IsAuthenticated 
 from django.db import DatabaseError 
+from rest_framework import generics
+from .models import Accidente
 
 class AccidenteCreateView(APIView):
     # vista protegida
@@ -27,4 +29,25 @@ class AccidenteCreateView(APIView):
             return Response({"CODE_ERR": "SERVER_ERROR"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response({"CODE_ERR": "Datos no ingresados correctamente"}, status=status.HTTP_400_BAD_REQUEST)
 
+class AccidenteListView(generics.ListAPIView):
+    serializer_class = AccidenteListSerializer
 
+    def get_queryset(self):
+        # Filtrar solo los accidentes confirmados
+        return Accidente.objects.filter(confirmado=True)
+
+    def list(self, request, *args, **kwargs):
+        """
+        Sobrescribe el método `list` para incluir el conteo de accidentes
+        confirmados en la respuesta.
+        """
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        # Contamos cuántos accidentes se están listando
+        count = queryset.count()
+
+        # Incluimos el conteo en la respuesta
+        return Response({
+            'count': count,
+            'accidentes': serializer.data
+        })
